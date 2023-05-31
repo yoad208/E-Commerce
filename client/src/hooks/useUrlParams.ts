ee09b85f53e09d827613b4530d0f98becc4d83aa
yoad208 <yoad208@gmail.com>
2023-05-31 20:53:05 +0300

    I fix the category filter on navigation bar to send to current location

--- client/src/hooks/useUrlParams.ts
+++ client/src/hooks/useUrlParams.ts
@@ -7,17 +7,19 @@ export const useUrlParams = (name: string, value: string) => {
     const {location, query} = useQueryString();
     const [urlParams, setUrlParams] = useState<string>(query?.get(name) || value);
 
+    const locationPathname = location.pathname !== "/" ? location.pathname : "/store";
+
     useMemo(() => setUrlParams(value), [name, value]);
 
     useEffect(() => {
         if (!urlParams) {
             query.delete(name);
-            navigate(`${location.pathname}?${query.toString()}`)
+            navigate(`${locationPathname}?${query.toString()}`)
         } else {
             query.set(name, urlParams);
         }
         if (query.toString().length > 0) {
-            navigate(`${location.pathname}?${query.toString()}`)
+            navigate(`${locationPathname}?${query.toString()}`)
         }
     }, [urlParams]);
 
